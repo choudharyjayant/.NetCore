7decd3b6ce0838318c268d98b36332ac503427b8
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-09-21 14:03:05 +0530

    Update rmBeforeInstall.sh

--- Dependency_Scripts/rmBeforeInstall.sh
+++ Dependency_Scripts/rmBeforeInstall.sh
@@ -1,3 +1,4 @@
-#! /bin/bash
-sudo rm -rf  /home/ubuntu/dotnet/*
-echo "I can "write" without double quotes"
+#!/bin/bash
+cd /home/ubuntu/dotnet/
+for pid in $(lsof -t -i:8000); do                        kill -9 $pid;                done
+sudo rm -rf *
